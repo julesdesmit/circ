3ac949c1b59384b21b7e0dfbd7f9931734169691
kwantam <kwantam@gmail.com>
2021-12-13 15:59:23 -0500

    add cfg to switch to bn254 curve

--- src/front/zsharp/term.rs
+++ src/front/zsharp/term.rs
@@ -11,14 +11,27 @@ use crate::circify::{CirCtx, Embeddable};
 use crate::ir::opt::cfold::fold as constant_fold;
 use crate::ir::term::*;
 
+// The modulus for Z#.
+// TODO: handle this better!
+#[cfg(feature = "bls12381")]
 lazy_static! {
-    // TODO: handle this better
-    /// The modulus for Z#.
     static ref ZSHARP_MODULUS: Integer = Integer::from_str_radix(
-        "52435875175126190479447740508185965837690552500527637822603658699938581184513",
+        "52435875175126190479447740508185965837690552500527637822603658699938581184513", // BLS12-381 group order
         10
     )
     .unwrap();
+}
+
+#[cfg(not(feature = "bls12381"))]
+lazy_static! {
+    static ref ZSHARP_MODULUS: Integer = Integer::from_str_radix(
+        "21888242871839275222246405745257275088548364400416034343698204186575808495617", // BN-254 group order
+        10
+    )
+    .unwrap();
+}
+
+lazy_static! {
     /// The modulus for Z#, as an ARC
     pub static ref ZSHARP_MODULUS_ARC: Arc<Integer> = Arc::new(ZSHARP_MODULUS.clone());
 }
